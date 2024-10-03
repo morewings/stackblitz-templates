32108c1fc1ee411e568f93865fe12fbb4bb6c205
Dima Vyshniakov <morewings@gmail.com>
2024-10-03 23:19:18 +0200

    add eslint rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import classes from './App.module.css'
+
 const App = () => {
-  return <div className="flex flex-col gap-3">Hello, world!</div>;
+  return <div className={classes.app}>Hello, <span className={classes.appInternal}>world</span>!</div>;
 };
 
 export default App;
