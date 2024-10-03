7ee292b9c42e509d33eb395e4797b160897d706b
Dima Vyshniakov <morewings@gmail.com>
2024-10-03 23:19:18 +0200

    add eslint rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import classes from './App.module.css'
+
 const App = () => {
-  return <div>Hello, world!</div>;
+  return <div className={classes.app}>Hello, <span className={classes.appInternal}>world</span>!</div>;
 };
 
 export default App;
